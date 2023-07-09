8e4c3a2210be0cd5f6f8b6440e96f0e4024249f1
Deepakpal-007 <123586552+Deepakpal-007@users.noreply.github.com>
2023-07-09 08:02:06 +0530

    feat - added Markedjs

--- src/Components/Markdown.js
+++ src/Components/Markdown.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import './Markdown.css'
+import { marked } from 'marked';
+
+
+
 export default function Markdown() {
+    
+    const onTyping = () =>{
+        const editorText = document.getElementById("editor")
+        const previewText = document.getElementById("preview")
+        previewText.innerHTML = marked.parse(editorText.value)
+        
+        console.log()
+    }
+    
+
+
+
   return (
     <div className="container">
       <div className="editor-box">
           <p className="heading-editor ">Editor</p>
-          <textarea name="editor" id="editor" cols="64" rows="10" onkeyup="updatePreview()"
-            className="textarea"/>
+          <textarea name="editor" id="editor" cols="64" rows="10" 
+            className="textarea" onKeyUp={onTyping}/>
       </div>
       <div className="preview-box">
         <p className="heading-preview">Output</p>
